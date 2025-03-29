f726ae9deb079b746e6d2b5a8c8b23d87b339429
Rayyan Zafier Leksono <140869637+Swook1@users.noreply.github.com>
2025-03-29 14:55:33 +0700

    About Us Page Done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Routes, Route, Navigate } from 'react-router-dom'
 import './App.css';
 import SignUp from './views/sign_up/SignUp';
 import SignIn from './views/sign_in/SignIn';
+import AboutUs from './views/about_us/AboutUs';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Route path='/' element={<Navigate to="/sign-in" />} />
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/about-us' element={<AboutUs />} />
       </Routes>
     </>
   )
